3da5b88dec61105cddd37eb03d8d0ea2dc6cf23d
Karan <karan.ekarigar@gmail.com>
2025-04-21 17:45:56 +0530

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import "@/css/satoshi.css";
+import "@/css/style.css";
+
+import { Sidebar } from "@/components/Layouts/sidebar";
+
+import "flatpickr/dist/flatpickr.min.css";
+import "jsvectormap/dist/jsvectormap.css";
+
+import { Header } from "@/components/Layouts/header";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import type { PropsWithChildren } from "react";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | NextAdmin - Next.js Dashboard Kit",
+    default: "NextAdmin - Next.js Dashboard Kit",
+  },
+  description:
+    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="h-screen overflow-hidden">
+        <Providers>
+          <NextTopLoader color="#5750F1" showSpinner={false} />
+          <main className="h-full w-full overflow-hidden p-4 md:p-6 2xl:p-10">
+            {children}
+          </main>
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
---
+++ src/components/Layouts/sidebar/data/index.ts
@@ -0,0 +1,101 @@
+import * as Icons from "../icons";
+
+export const NAV_DATA = [
+  {
+    label: "MAIN MENU",
+    items: [
+      {
+        title: "Dashboard",
+        url: "/admin",
+        icon: Icons.HomeIcon,
+        items: [],
+      },
+      // {
+      //   title: "Calendar",
+      //   url: "/admin/calendar",
+      //   icon: Icons.Calendar,
+      //   items: [],
+      // },
+      // {
+      //   title: "Profile",
+      //   url: "/admin/profile",
+      //   icon: Icons.User,
+      //   items: [],
+      // },
+      {
+        title: "Add Employee",
+        icon: Icons.Alphabet,
+        items: [
+          {
+            title: "Form Elements",
+            url: "/admin/forms/form-elements",
+          },
+          {
+            title: "Form Layout",
+            url: "/admin/forms/form-layout",
+          },
+        ],
+      },
+      // {
+      //   title: "Tables",
+      //   url: "/tables",
+      //   icon: Icons.Table,
+      //   items: [
+      //     {
+      //       title: "Tables",
+      //       url: "/tables",
+      //     },
+      //   ],
+      // },
+      {
+        title: "Pages",
+        icon: Icons.Alphabet,
+        items: [
+          {
+            title: "Settings",
+            url: "/admin/pages/settings",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "OTHERS",
+    items: [
+      {
+        title: "Charts",
+        icon: Icons.PieChart,
+        items: [
+          {
+            title: "Basic Chart",
+            url: "/admin/charts/basic-chart",
+          },
+        ],
+      },
+      {
+        title: "UI Elements",
+        icon: Icons.FourCircle,
+        items: [
+          {
+            title: "Alerts",
+            url: "/admin/ui-elements/alerts",
+          },
+          {
+            title: "Buttons",
+            url: "/admin/ui-elements/buttons",
+          },
+        ],
+      },
+      {
+        title: "Authentication",
+        icon: Icons.Authentication,
+        items: [
+          {
+            title: "Sign In",
+            url: "/auth/sign-in",
+          },
+        ],
+      },
+    ],
+  },
+];
